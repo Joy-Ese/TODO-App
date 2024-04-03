bbcc5900f774cbea2b85eb5c863eff199ef36298
Joy-Ese <86070579+Joy-Ese@users.noreply.github.com>
2024-04-03 17:04:51 +0100

    Feat: Set up MongoDb. Create "ToDoAppDB" database. Add a collection(table) called "User". Add: Set up AuthController, AuthService and IAuth.

---
+++ TODO.Services/Services/AuthServices.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using TODO.Services.Interfaces;
+
+namespace TODO.Services.Services
+{
+    public class AuthServices : IAuth
+    {
+        private readonly ILogger<AuthServices> _logger;
+
+        public AuthServices(ILogger<AuthServices> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "Nlog injected into AuthServices");
+        }
+
+        public string AccountNumberGenerator()
+        {
+            var accountNumber = DateTime.Now.ToString("HHffMMffdd");
+            return accountNumber;
+        }
+
+    }
+}
